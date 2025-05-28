ee0abbb0287c9f7a4d9554c79b893088f42dcb7a
Harnoorsoftware <harnoorsoftware@gmail.com>
2025-05-28 14:08:05 +0530

    Create app_3.py

---
+++ app_3.py
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<style>
+table, th, td {
+  border:1px solid black;
+  border-collapse: collapse;
+}
+</style>
+<body>
+
+<h1>Hi! I am Harnoor!
+<h2>Welcome to my <em>third blog.</em></h2>
+<h3>Recipe to tea(Well, I had enough of story blogs so I decided to start a cooking blog!!!)</h3>
+<h4>Materials : a utensil for making tea, 2 cups water and 1 milk, tea leaves and sugar (measurements as per your taste),ginger, cardamom , a tea strainer and cup .
+
+<table style="width:100%">
+  <tr>
+    <th style="width:70%">Steps</th>
+    <th>Images</th> 
+  </tr>
+  <tr>
+    <td>Pour 2 cups of water into the utensil.</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>Add tea leaves, sugar, ginger and cardamom and let it boil.</td>
+    <td>Jackson</td>
+  </tr>
+  <tr>
+    <td>After boiling it ,add milk to it and let it boil thoroughly.</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>Lastly, stain it in a cup and enjoy!</td>
+    <td></td>
+  </tr>
+
+</table>
+
+</body>
+</html>
+
